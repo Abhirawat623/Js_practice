2feeee427b1172d2111de170474ae2e6d0f5da41
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2024-03-05 23:04:30 +0530

    Create integer to roman.js

---
+++ integer to roman.js
@@ -0,0 +1,39 @@
+// Function to convert decimal to Roman Numerals
+
+function printRoman(number)
+{
+
+    let num = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+
+    let sym = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+
+    let i=12;
+
+    while(number>0)
+
+    {
+
+    let div = Math.floor(number/num[i]);
+
+    number = number%num[i];
+
+    while(div--)
+
+    {
+
+        document.write(sym[i]);
+
+    }
+
+    i--;
+
+    }
+}
+ 
+//Driver program
+ 
+
+    let number = 3549;
+ 
+
+    printRoman(number);
